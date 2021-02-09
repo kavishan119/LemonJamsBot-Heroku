aa140cf2c8c0947eec2cdfba76e06256efa068e9
kavishan119 <77564996+kavishan119@users.noreply.github.com>
2021-02-09 20:39:37 +0530

    Update help.ts

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -5,9 +5,6 @@ export const playHandler = Composer.command('help', async ctx => {
     const { chat } = ctx.message;
 
     if (chat.type !== 'supergroup') {
-        await ctx.reply('🔸මෙම bot ඔබගේ group එකට add කරන්න.
-🔸bot ව Admin කෙනෙක් කරන්න.(Remain Anonymous හැර සියලු permissions ලබා දෙන්න.
-🔸දැන් group එක තුල /play <space> <අදාල youtube Link එක දාන්න>.
-Enjoy..❕');
+        await ctx.reply('🔸මෙම bot ඔබගේ group එකට add කරන්න.🔸bot ව Admin කෙනෙක් කරන්න.(Remain Anonymous හැර සියලු permissions ලබා දෙන්න.🔸දැන් group එක තුල /play <space> <අදාල youtube Link එක දාන්න>.Enjoy..❕');
         return;
     }
